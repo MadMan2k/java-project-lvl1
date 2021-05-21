510c1f4d8b55f6219b5cf3dec48dc4409bfb98c7
igor.onuferko <igor.nfrk@gmail.com>
2021-05-21 23:18:45 +0200

    fix checkstyle v3

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -7,21 +7,28 @@ public class App {
         String welcome = "Welcome to the Brain Games!";
         final int numberOfChoice = 4;
         String[] availableChoice = new String[numberOfChoice];
-        availableChoice[0] = "0 - Exit";
-        availableChoice[1] = "1 - Greet";
-        availableChoice[2] = "2 - Even";
-        availableChoice[3] = "3 - Calc";
+        final int exit0 = 0;
+        final int greet1 = 1;
+        final int even2 = 2;
+        final int calc3 = 3;
+        availableChoice[exit0] = "0 - Exit";
+        availableChoice[greet1] = "1 - Greet";
+        availableChoice[even2] = "2 - Even";
+        availableChoice[calc3] = "3 - Calc";
         System.out.println("Please enter the game number and press Enter.");
         for (int i = 1; i < availableChoice.length; i++) {
             System.out.println(availableChoice[i]);
         }
-        System.out.println(availableChoice[0]);
+        System.out.println(availableChoice[exit0]);
         Scanner setGame = new Scanner(System.in);
         System.out.print("Your choice : ");
         String choosedGame = setGame.nextLine();
         System.out.println();
 
         switch (choosedGame) {
+            case "0":
+                System.out.println("Not ready, TODO");
+                break;
             case "1":
                 System.out.println(welcome);
                 Cli.setName();
