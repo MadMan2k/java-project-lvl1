4a80d7c686120ad46ac227cda2e37bb1dd9bda10
igor.onuferko <igor.nfrk@gmail.com>
2021-08-08 00:25:39 +0200

    test

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -9,6 +9,7 @@ import hexlet.code.games.Progression;
 import java.util.Scanner;
 
 public class App {
+//    test
     public static void main(String[] args) {
         final int exit0 = 0;
         final int greet1 = 1;
