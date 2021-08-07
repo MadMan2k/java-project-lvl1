5cb2374fe805958d80010b17086b694bf79ea7ec
igor.onuferko <igor.nfrk@gmail.com>
2021-08-07 21:59:08 +0200

    test3

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -55,6 +55,7 @@ public class Progression {
 //public static void main(String[] args) {
 //    progressionGame();
 //}
+    
 
     public static void progressionGame() {
         String[] gameQuestions = new String[Engine.getNumberOfTries()];
