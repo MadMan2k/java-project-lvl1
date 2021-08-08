c6e07a0f1b8d172edfcbe82e3b6acb69cd95fc25
igor.onuferko <igor.nfrk@gmail.com>
2021-08-08 15:19:36 +0200

    calc optimization

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -4,7 +4,7 @@ import hexlet.code.Engine;
 
 public class Calc {
     private static final String RULES = "What is the result of the expression?";
-    private static final int DECREASE_DIFFICULTY_CALC = 5;
+    private static final int DECREASE_DIFFICULTY_CALC = 10;
 //    public static void calcGame() {
 //        final int numberOfOperators = 3;
 //        int resultInt = 0;
@@ -46,35 +46,28 @@ public class Calc {
     public static void calcGame() {
         String[] gameQuestions = new String[Engine.NUMBER_OF_TRIES];
         String[] rightAnswers = new String[Engine.NUMBER_OF_TRIES];
-        final int numberOfOperators = 3;
         int[] resultInt = new int[Engine.NUMBER_OF_TRIES];
         int[][] randomNumArrays = new int[Engine.NUMBER_OF_TRIES][2];
         for (int i = 0; i < randomNumArrays.length; i++) {
             randomNumArrays[i] = Engine.getRandomNumArray();
         }
-        int[] showRandomOperators = new int[Engine.NUMBER_OF_TRIES];
-        for (int j = 0; j < showRandomOperators.length; j++) {
-            showRandomOperators[j] = Engine.getRandomNum().nextInt(numberOfOperators);
-        }
+        int[] randomOperators = getRandomOperators(); // 0 ==> sum ; 1 ==> subtraction ; 2 ==>  product ;
         for (int k = 0; k < randomNumArrays.length; k++) {
-            switch (showRandomOperators[k]) { // 0 ==> sum ; 1 ==> subtraction ; 2 ==>  product ; default ==> error
+            switch (randomOperators[k]) { // 0 ==> sum ; 1 ==> subtraction ; 2 ==>  product ; default ==> error
                 case 0:
-                    gameQuestions[k] = String.valueOf(randomNumArrays[k][0]) + " + "
-                            + String.valueOf(randomNumArrays[k][1]);
+                    gameQuestions[k] = gameQuestion(randomNumArrays[k][0], randomNumArrays[k][1], "+");
                     resultInt[k] = sum(randomNumArrays[k][0], randomNumArrays[k][1]);
-                    rightAnswers[k] = String.valueOf(resultInt[k]);
+                    rightAnswers[k] = resultToString(resultInt[k]);
                     break;
                 case 1:
-                    gameQuestions[k] = String.valueOf(randomNumArrays[k][0]) + " - "
-                            + String.valueOf(randomNumArrays[k][1]);
+                    gameQuestions[k] = gameQuestion(randomNumArrays[k][0], randomNumArrays[k][1], "-");
                     resultInt[k] = subtraction(randomNumArrays[k][0], randomNumArrays[k][1]);
-                    rightAnswers[k] = String.valueOf(resultInt[k]);
+                    rightAnswers[k] = resultToString(resultInt[k]);
                     break;
-                case 2: // The difficulty decreased to one-fifth of its original level for easier game
-                    gameQuestions[k] = String.valueOf(randomNumArrays[k][0] / DECREASE_DIFFICULTY_CALC) + " * "
-                            + String.valueOf(randomNumArrays[k][1] / DECREASE_DIFFICULTY_CALC);
+                case 2: // The difficulty decreased to one-tenth of its original level for easier game
+                    gameQuestions[k] = gameQuestion(randomNumArrays[k][0], randomNumArrays[k][1], "*");
                     resultInt[k] = productDifficultyDecreased(randomNumArrays[k][0], randomNumArrays[k][1]);
-                    rightAnswers[k] = String.valueOf(resultInt[k]);
+                    rightAnswers[k] = resultToString(resultInt[k]);
                     break;
                 default :
                     Engine.printError();
@@ -83,15 +76,47 @@ public class Calc {
         Engine.runGame(RULES, gameQuestions, rightAnswers);
     }
 
-    public static int sum(int firstNumber, int secondNumber) {
+    private static String resultToString(int rightAnswer) {
+        return String.valueOf(rightAnswer);
+    }
+
+    private static String gameQuestion(int firstNum, int secondNum, String operator) {
+        String gameQuestion;
+        switch (operator) {
+            case "+":
+                gameQuestion = String.format("%s + %s", firstNum, secondNum);
+                break;
+            case "-":
+                gameQuestion = String.format("%s - %s", firstNum, secondNum);
+                break;
+            case "*":
+                gameQuestion = String.format("%s * %s", firstNum / DECREASE_DIFFICULTY_CALC,
+                        secondNum / DECREASE_DIFFICULTY_CALC);
+                break;
+            default:
+                gameQuestion = "Error -1";
+        }
+        return gameQuestion;
+    }
+
+    private static int[] getRandomOperators() {
+        final int numberOfOperators = 3;
+        int[] randomOperators = new int[Engine.NUMBER_OF_TRIES];
+        for (int j = 0; j < randomOperators.length; j++) {
+            randomOperators[j] = Engine.getRandomNum().nextInt(numberOfOperators);
+        }
+        return randomOperators;
+    }
+
+    private static int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
     }
 
-    public static int subtraction(int firstNumber, int secondNumber) {
+    private static int subtraction(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
     }
 
-    public static int productDifficultyDecreased(int firstNumber, int secondNumber) {
+    private static int productDifficultyDecreased(int firstNumber, int secondNumber) {
         return (firstNumber / DECREASE_DIFFICULTY_CALC) * (secondNumber / DECREASE_DIFFICULTY_CALC);
     }
 }
