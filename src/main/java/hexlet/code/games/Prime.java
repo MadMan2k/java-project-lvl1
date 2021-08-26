6c70017ded416fb7c1a92ed0dfef0f6c155c4f0f
igor.onuferko <igor.nfrk@gmail.com>
2021-08-26 14:22:22 +0200

    Prime.java checkRightAnswer() rework, randomNum generation rework

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -5,29 +5,32 @@ import hexlet.code.Utils;
 
 public class Prime {
     private static final String RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    private static final int THREE_IS_NOT_MAGIC_NUMBER = 3;
     public static void primeGame() {
         String[] gameQuestions = new String[Engine.NUMBER_OF_TRIES];
         String[] rightAnswers = new String[Engine.NUMBER_OF_TRIES];
-        final int threeIsNotMagicNumber = 3;
-        int[] randomNums = Utils.getRandomNumsArray(Engine.NUMBER_OF_TRIES);
         for (int i = 0; i < Engine.NUMBER_OF_TRIES; i++) {
-            gameQuestions[i] = String.valueOf(randomNums[i]);
-            if (randomNums[i] == 0 || randomNums[i] == 1) {
-                rightAnswers[i] = Engine.NO_TEXT;
-            }
-            if (randomNums[i] == 2 || randomNums[i] == threeIsNotMagicNumber) {
-                rightAnswers[i] = Engine.YES_TEXT;
-            }
-            rightAnswers[i] = checkRightAnswer(randomNums[i]);
+            int randomNum = Utils.getRandomNum(Utils.MAX_OF_RANDOM);
+            gameQuestions[i] = String.valueOf(randomNum);
+            rightAnswers[i] = checkRightAnswer(randomNum);
         }
         Engine.runGame(RULES, gameQuestions, rightAnswers);
     }
 
     private static String checkRightAnswer(int randomNum) {
-        String rightAnswer = Engine.YES_TEXT;
+        String rightAnswer = "";
+        if (randomNum == 0 || randomNum == 1) {
+            rightAnswer = Engine.NO_TEXT;
+        }
+        if (randomNum == 2 || randomNum == THREE_IS_NOT_MAGIC_NUMBER) {
+            rightAnswer = Engine.YES_TEXT;
+        }
         for (int k = 2; k <= randomNum / 2; k++) {
             if ((randomNum % k) == 0) {
                 rightAnswer = Engine.NO_TEXT;
+                break;
+            } else {
+                rightAnswer = Engine.YES_TEXT;
             }
         }
         return rightAnswer;
