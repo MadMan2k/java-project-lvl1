dda8107fb6d1b7c13eea559daea5c19b7beb6b11
igor.onuferko <igor.nfrk@gmail.com>
2021-08-31 19:52:48 +0200

    Engine.java optimization

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -8,34 +8,27 @@ public class Engine {
     public static final int NUMBER_OF_TRIES = 3;
     public static final String YES_TEXT = "yes";
     public static final String NO_TEXT = "no";
-    private static int counterOfCorrectAnswers = 0;
-    private static final Scanner SET_ANSWER = new Scanner(System.in);
 
     public static void setPlayerName() {
         name = Cli.getNameFromPlayer();
     }
 
-    private static boolean verifyAnswer(String result, String answer) {
-        if (answer.equals(result)) {
-            System.out.println("Correct!");
-            counterOfCorrectAnswers++;
-            return true;
-        } else {
-            System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'%n", answer, result);
-            System.out.println("Let's try again, " + name + "!");
-            return false;
-        }
-    }
-
     public static void runGame(String rules, String[] gameQuestions, String[] rightAnswers) {
+        Scanner getAnswerFromPlayer = new Scanner(System.in);
+        int counterOfCorrectAnswers = 0;
         System.out.println(rules);
         for (int i = 0; i < NUMBER_OF_TRIES; i++) {
             System.out.print("Question: ");
             System.out.println(gameQuestions[i]);
             System.out.print("Your answer : ");
-            String playerAnswer = SET_ANSWER.nextLine();
-            if (!verifyAnswer(rightAnswers[i], playerAnswer)) {
+            String playerAnswer = getAnswerFromPlayer.nextLine();
+            if (!rightAnswers[i].equals(playerAnswer)) {
+                System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'%n", playerAnswer, rightAnswers[i]);
+                System.out.println("Let's try again, " + name + "!");
                 break;
+            } else {
+                System.out.println("Correct!");
+                counterOfCorrectAnswers++;
             }
         }
         if (counterOfCorrectAnswers == NUMBER_OF_TRIES) {
