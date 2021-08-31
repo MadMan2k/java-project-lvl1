f7aa251c34fb2ed2f7ae459b44bb85cbf6e3fc95
igor.onuferko <igor.nfrk@gmail.com>
2021-08-31 12:24:56 +0200

    Progression.java optimization

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -6,22 +6,22 @@ import hexlet.code.Utils;
 public class Progression {
     private static final String RULES = "What number is missing in the progression?";
     private static final int PROGRESSION_SIZE = 10;
+    private static final int MAX_STEP = 10;
 
     public static void progressionGame() {
-        final int maxCommonDifference = 10;
         String[] gameQuestions = new String[Engine.NUMBER_OF_TRIES];
         String[] rightAnswers = new String[Engine.NUMBER_OF_TRIES];
         for (int i = 0; i < Engine.NUMBER_OF_TRIES; i++) {
             int indexOfHiddenNum = Utils.getRandomNum(PROGRESSION_SIZE);
             int startNumOfProgression = Utils.getRandomNum(Utils.MAX_OF_RANDOM);
-            int commonDifference = Utils.getRandomNum(maxCommonDifference);
+            int step = Utils.getRandomNum(MAX_STEP);
             StringBuilder sb = new StringBuilder();
             for (int j = 0; j < PROGRESSION_SIZE; j++) {
                 if (j == indexOfHiddenNum) {
                     sb.append(".. ");
-                    rightAnswers[i] = String.valueOf(startNumOfProgression + (commonDifference * j));
+                    rightAnswers[i] = String.valueOf(startNumOfProgression + (step * j));
                 } else {
-                    sb.append(startNumOfProgression + (commonDifference * j)).append(" ");
+                    sb.append(startNumOfProgression + (step * j)).append(" ");
                 }
             }
             gameQuestions[i] = sb.toString();
