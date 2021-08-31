64862be2cd0520c2773eff9531b800bb9f4f8235
igor.onuferko <igor.nfrk@gmail.com>
2021-08-31 19:34:30 +0200

    Prime.java optimization

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -5,34 +5,26 @@ import hexlet.code.Utils;
 
 public class Prime {
     private static final String RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-    private static final int THREE_IS_NOT_MAGIC_NUMBER = 3;
     public static void primeGame() {
         String[] gameQuestions = new String[Engine.NUMBER_OF_TRIES];
         String[] rightAnswers = new String[Engine.NUMBER_OF_TRIES];
         for (int i = 0; i < Engine.NUMBER_OF_TRIES; i++) {
             int randomNum = Utils.getRandomNum(Utils.MAX_OF_RANDOM);
             gameQuestions[i] = String.valueOf(randomNum);
-            rightAnswers[i] = checkRightAnswer(randomNum);
+            rightAnswers[i] = isPrimeNumber(randomNum) ? Engine.YES_TEXT : Engine.NO_TEXT;
         }
         Engine.runGame(RULES, gameQuestions, rightAnswers);
     }
 
-    private static String checkRightAnswer(int randomNum) {
-        String rightAnswer = "";
-        if (randomNum == 0 || randomNum == 1) {
-            rightAnswer = Engine.NO_TEXT;
+    private static boolean isPrimeNumber(int randomNum) {
+        if (randomNum < 2) {
+            return false;
         }
-        if (randomNum == 2 || randomNum == THREE_IS_NOT_MAGIC_NUMBER) {
-            rightAnswer = Engine.YES_TEXT;
-        }
-        for (int k = 2; k <= randomNum / 2; k++) {
+        for (int k = 2; k <= Math.sqrt(randomNum); k++) {
             if ((randomNum % k) == 0) {
-                rightAnswer = Engine.NO_TEXT;
-                break;
-            } else {
-                rightAnswer = Engine.YES_TEXT;
+                return false;
             }
         }
-        return rightAnswer;
+        return true;
     }
 }
