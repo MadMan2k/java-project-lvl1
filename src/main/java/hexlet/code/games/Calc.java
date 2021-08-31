0a161d75ee4738be2048ed61fdf40582ba535618
igor.onuferko <igor.nfrk@gmail.com>
2021-08-31 12:18:32 +0200

    Calc.java optimization

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -3,59 +3,42 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
 
-import java.util.Random;
-
 public class Calc {
     private static final String RULES = "What is the result of the expression?";
     private static final int DECREASE_DIFFICULTY_CALC = 10;
-    private static final int[] OPERATORS = {0, 1, 2}; // 0 ==> sum; 1 ==> subtraction; 2 ==> product;
+    private static final String[] OPERATORS = {"+", "-", "*"};
     public static void calcGame() {
         String[] gameQuestions = new String[Engine.NUMBER_OF_TRIES];
         String[] rightAnswers = new String[Engine.NUMBER_OF_TRIES];
         for (int k = 0; k < Engine.NUMBER_OF_TRIES; k++) {
             int firstRandomNum = Utils.MIN_OF_RANDOM + Utils.getRandomNum(Utils.MAX_OF_RANDOM);
             int secondRandomNum = Utils.MIN_OF_RANDOM + Utils.getRandomNum(Utils.MAX_OF_RANDOM);
-            int randomOperator = OPERATORS[new Random().nextInt(OPERATORS.length)];
-            gameQuestions[k] = getGameQuestion(firstRandomNum, secondRandomNum, randomOperator);
-            rightAnswers[k] = getRightAnswer(randomOperator, firstRandomNum, secondRandomNum);
+            String randomOperator = OPERATORS[Utils.getRandomNum(OPERATORS.length)];
+            if (randomOperator.equals("*")) { // Difficulty is decreased to one-tenth of its original level for easier game
+                firstRandomNum = firstRandomNum / DECREASE_DIFFICULTY_CALC;
+                secondRandomNum = secondRandomNum / DECREASE_DIFFICULTY_CALC;
+            }
+            gameQuestions[k] = String.format("%s %s %s", firstRandomNum, randomOperator, secondRandomNum);
+            rightAnswers[k] = getRightAnswer(firstRandomNum, secondRandomNum, randomOperator);
         }
         Engine.runGame(RULES, gameQuestions, rightAnswers);
     }
 
-    private static String getRightAnswer(int randomOperator, int firstRandomNum, int secondRandomNum) {
-        int resultInt = 0;
-        switch (randomOperator) { // 0 ==> sum; 1 ==> subtraction; 2 ==> product; default ==> error
-            case 0:
+    private static String getRightAnswer(int firstRandomNum, int secondRandomNum, String randomOperator) {
+        int resultInt;
+        switch (randomOperator) {
+            case "+":
                 resultInt = firstRandomNum + secondRandomNum;
                 break;
-            case 1:
+            case "-":
                 resultInt = firstRandomNum - secondRandomNum;
                 break;
-            case 2: // The difficulty decreased to one-tenth of its original level for easier game
-                resultInt = (firstRandomNum / DECREASE_DIFFICULTY_CALC) * (secondRandomNum / DECREASE_DIFFICULTY_CALC);
+            case "*":
+                resultInt = firstRandomNum * secondRandomNum;
                 break;
             default :
                 return "Error -1";
         }
         return String.valueOf(resultInt);
-    }
-
-    private static String getGameQuestion(int firstNum, int secondNum, int operator) {
-        String gameQuestion;
-        switch (operator) { // 0 ==> sum; 1 ==> subtraction; 2 ==> product;
-            case 0:
-                gameQuestion = String.format("%s + %s", firstNum, secondNum);
-                break;
-            case 1:
-                gameQuestion = String.format("%s - %s", firstNum, secondNum);
-                break;
-            case 2:
-                gameQuestion = String.format("%s * %s", firstNum / DECREASE_DIFFICULTY_CALC,
-                        secondNum / DECREASE_DIFFICULTY_CALC);
-                break;
-            default:
-                gameQuestion = "Error -1";
-        }
-        return gameQuestion;
     }
 }
