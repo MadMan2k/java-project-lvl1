1e2d39d561f23fbe015102accf2a0a80000274b0
igor.onuferko <igor.nfrk@gmail.com>
2021-08-31 10:47:41 +0200

    App.java optimization

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -9,14 +9,49 @@ import hexlet.code.games.Progression;
 import java.util.Scanner;
 
 public class App {
+    final static int EXIT_0 = 0;
+    final static int GREET_1 = 1;
+    final static int EVEN_2 = 2;
+    final static int CALC_3 = 3;
+    final static int GCD_4 = 4;
+    final static int PROGRESSION_5 = 5;
+    final static int PRIME_6 = 6;
+
     public static void main(String[] args) {
-        final int exit0 = 0;
-        final int greet1 = 1;
-        final int even2 = 2;
-        final int calc3 = 3;
-        final int gcd4 = 4;
-        final int progression5 = 5;
-        final int prime6 = 6;
+        printGameMenu();
+        System.out.println("Please enter the game number and press Enter.");
+        Scanner setGame = new Scanner(System.in);
+        System.out.print("Your choice : ");
+        int selectedGame = setGame.nextInt();
+        System.out.println();
+        if (selectedGame != EXIT_0) {
+            System.out.println("Welcome to the Brain Games!");
+            Engine.setPlayerName();
+            switch (selectedGame) {
+                case GREET_1:
+                    break;
+                case EVEN_2:
+                    Even.evenGame();
+                    break;
+                case CALC_3:
+                    Calc.calcGame();
+                    break;
+                case GCD_4:
+                    GCD.gcdGame();
+                    break;
+                case PROGRESSION_5:
+                    Progression.progressionGame();
+                    break;
+                case PRIME_6:
+                    Prime.primeGame();
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
+
+    private static void printGameMenu() {
         System.out.println("0 - Exit");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
@@ -24,35 +59,5 @@ public class App {
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
         System.out.println("6 - Prime");
-        System.out.println("Please enter the game number and press Enter.");
-        Scanner setGame = new Scanner(System.in);
-        System.out.print("Your choice : ");
-        int selectedGame = setGame.nextInt();
-        System.out.println();
-        if (selectedGame != exit0) {
-            System.out.println("Welcome to the Brain Games!");
-            Engine.setPlayerName();
-            switch (selectedGame) {
-                case greet1:
-                    break;
-                case even2:
-                    Even.evenGame();
-                    break;
-                case calc3:
-                    Calc.calcGame();
-                    break;
-                case gcd4:
-                    GCD.gcdGame();
-                    break;
-                case progression5:
-                    Progression.progressionGame();
-                    break;
-                case prime6:
-                    Prime.primeGame();
-                    break;
-                default:
-                    System.out.println("Invalid choice");
-            }
-        }
     }
 }
